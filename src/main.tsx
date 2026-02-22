65b6b31dd7640503631ffa5b02ac19e5a766e5bb
Phlog0 <83307866+Phlog0@users.noreply.github.com>
2026-02-23 01:09:45 +0300

    кривой код чтобы проверить husky

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./index.css";
-// const x=1;const y=  2;console.log( x+y);
-// const userData: any = { id: 1 };
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
